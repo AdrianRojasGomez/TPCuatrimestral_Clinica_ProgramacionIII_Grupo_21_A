5a9f832bf5f2e22cc5a15c0098cf83d1f7456225
Adrian <arojasgomez.93@gmail.com>
2025-11-08 19:48:32 -0300

    VIsual Fixes Gestion

--- TPCuatrimestralClinica/Dominio/Turno.cs
+++ TPCuatrimestralClinica/Dominio/Turno.cs
@@ -8,17 +8,19 @@ namespace Dominio
 {
     public class Turno
     {
-
         public int IdTurno { get; set; }
-        public int NumeroTurno { get; set; }
-        public DateTime FechaHora { get; set; }
+        public string NumeroTurno { get; set; }
+        public DateTime FechaInicio { get; set; }
+        public DateTime FechaFin { get; set; }
+        public TimeSpan HoraInicio { get; set; }
+        public TimeSpan HoraFin { get; set; }
         public string ObservacionesSolicitud { get; set; }
         public string ObservacionesDiagnostico { get; set; }
         public Paciente Paciente { get; set; }
         public Medico Medico { get; set; }
         public Especialidad Especialidad { get; set; }
-        public EstadoTurno Estado { get; set; }
-
+        public string Motivo { get; set; }
+        public bool Estado { get; set; }
 
     }
 }
---
+++ TPCuatrimestralClinica/WebApplicationClinica/Turnos/GestionTurno.aspx.cs
@@ -0,0 +1,35 @@
+﻿using Dominio;
+using Negocio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplicationClinica.Turnos
+{
+    public partial class GestionTurno : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            CargarTurnos();
+        }
+
+        void CargarTurnos(string filtro = "")
+        {
+            try
+            {
+                TurnoNegocio negocio = new TurnoNegocio();
+                var dt = negocio.Listar(filtro);
+                gvTurnos.DataSource = dt;
+                gvTurnos.DataBind();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al listar pacientes: " + ex.Message);
+            }
+        }
+
+    }
+}

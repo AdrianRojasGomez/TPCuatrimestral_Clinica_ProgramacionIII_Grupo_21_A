16dc296f676bb1effa39cbfb1941354eb28a7351
Adrian <arojasgomez.93@gmail.com>
2025-11-22 13:01:10 -0300

    enum estadoturno

---
+++ TPCuatrimestralClinica/Dominio/EstadoTurno.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class EstadoTurno
+    {
+        public enum EstadoEnum
+        {
+            Pendiente = 1,
+            Confirmado = 2,
+            Completado = 3,
+            NoAsistio = 4,
+            Cancelado = 0
+        }
+
+        public EstadoEnum Estado { get; set; }
+    }
+}
--- TPCuatrimestralClinica/WebApplicationClinica/Medicos/MenuMedico.aspx.cs
+++ TPCuatrimestralClinica/WebApplicationClinica/Medicos/MenuMedico.aspx.cs
@@ -14,13 +14,14 @@ namespace WebApplicationClinica.Medicos
         {
             if (!IsPostBack)
             {
-                if (Session["idMedico"] == null)
-                {
-                    Response.Redirect("~/Login%20y%20Usuarios/Login.aspx");
-                    return;
-                }
+                //if (Session["idMedico"] == null)
+                //{
+                //    Response.Redirect("~/Login%20y%20Usuarios/Login.aspx");
+                //    return;
+                //}
 
-                int idMedico = (int)Session["idMedico"];
+                //int idMedico = (int)Session["idMedico"];
+                int idMedico = 1; // Temporal hasta login
 
                 CargarDatosDoctor(idMedico);
 
